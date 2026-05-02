0f32cf6d7ff98fcd8b1c6a33418f55daea6f3580
tomatoo11 <riitiippatsu.doradora@gmail.com>
2026-05-02 15:39:33 +0900

    好きな食べ物を追加

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const hobbies = ["NFT", "ゲーム", "酒"];
+const foods = ["お好み焼き", "火鍋", "海鮮"];
 
 const hobbyList = document.querySelector("#hobby-list");
+const foodList = document.querySelector("#food-list");
 
 hobbies.forEach((hobby) => {
   const item = document.createElement("li");
   item.textContent = hobby;
   hobbyList.appendChild(item);
+});
+
+foods.forEach((food) => {
+  const item = document.createElement("li");
+  item.textContent = food;
+  foodList.appendChild(item);
 });
